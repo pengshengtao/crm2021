c69e0da381c943b116094f34fc00e479759ef91c
pst <1264531161@qq.com>
2021-08-17 17:12:12 +0800

    one

---
+++ crm/src/main/java/com/bjpowernode/crm/workbench/service/ClueService.java
@@ -0,0 +1,22 @@
+package com.bjpowernode.crm.workbench.service;
+
+import com.bjpowernode.crm.workbench.domain.Clue;
+import com.bjpowernode.crm.workbench.domain.Tran;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Author 北京动力节点
+ */
+public interface ClueService {
+    boolean save(Clue c);
+
+    Clue detail(String id);
+
+    boolean unbund(String id);
+
+    boolean bund(String cid, String[] aids);
+
+
+    boolean convert(String clueId, Tran t, String createBy);
+}
